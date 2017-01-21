fc48a0c6d8c71d0125d3162001cd0bb5f463cc86
Scott Munro <scottnmunro@gmail.com>
2017-01-21 02:44:20 -0600

    added a simple grid generator and a character that can move around using input on the screen

---
+++ Assets/Scenes/TestAssets/Movement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        Vector3 motionVector = InputWrapper.GetLeftStickVector();
+        transform.Translate(motionVector * Time.deltaTime);
+	}
+}
