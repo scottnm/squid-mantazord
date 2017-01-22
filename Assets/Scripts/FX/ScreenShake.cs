e57716dc483a1510ed96a8ef95a06befd79b5688
Scott Munro <scottnmunro@gmail.com>
2017-01-22 00:57:46 -0600

    shake before wave starts

---
+++ Assets/Scripts/FX/ScreenShake.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShake : MonoBehaviour
+{
+    [SerializeField]
+    float shakeTime;
+    [SerializeField]
+    float shakeSpeed;
+    [SerializeField]
+    float shakeIntensity;
+
+    private void OnWaveStart()
+    {
+        StartCoroutine(Shake());
+    }
+
+    public IEnumerator Shake()
+    {
+        Debug.Log("ShakeStart");
+        var cameraOrigin = transform.position;
+        float time = 0;
+        while (time < shakeTime)
+        {
+            float shakeOffset = shakeIntensity * Mathf.Sin(shakeSpeed * time);
+            transform.position = cameraOrigin + Vector3.right * shakeOffset;
+            yield return null;
+            time += Time.deltaTime;
+        }
+        transform.position = cameraOrigin;
+    }
+}
