1aad5ab86846343669113b8f0dbd0a9087de85ce
Scott Munro <scottnmunro@gmail.com>
2017-01-21 19:52:34 -0600

    worbldy gorbldy ai

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -7,8 +7,8 @@ public class EnemyAI : MonoBehaviour
     enum AiAction
     {
         GoForward,
-        TurnAndMove,
-        Turn,
+        TurnLeft,
+        TurnRight,
         DoNothing
     }
 
@@ -16,39 +16,51 @@ public class EnemyAI : MonoBehaviour
     public struct AiAttitude
     {
         public float GoForwardRatio;
-        public float TurnAndMoveRatio;
-        public float TurnRatio;
+        public float TurnLeftRatio;
+        public float TurnRightRatio;
         public float DoNothingRatio;
     }
 
+    [System.Serializable]
+    public struct AiMovement
+    {
+        public float moveSpeed;
+        public float turnSpeed;
+    }
+
+    [System.Serializable]
     struct GameStateEvaluation
     {
         public Vector2 directionToPlayer;
         public bool canSeePlayer;
         public Vec2i tilePos;
-        public bool canMoveUp;
+        public bool canMoveForward;
+        public bool canMoveBack;
         public bool canMoveRight;
         public bool canMoveLeft;
-        public bool canMoveDown;
     }
 
+    [SerializeField]
+    AiMovement movement;
     [SerializeField]
     private AiAttitude attitude;
     [SerializeField]
     private AiAction currentAction = AiAction.DoNothing;
     [SerializeField]
     private float aiCooldownTime;
-    [SerializeField]
     private float aiCooldownTimer;
     [SerializeField]
-    private int actionCounts = 0;
     private Vector2 facingVector;
+    [SerializeField]
+    private Vector2 movementVector;
+    [SerializeField]
     private GameStateEvaluation currentState;
     private GameObject player;
 
 	private void Start ()
     {
         facingVector = Vector2.up;
+        movementVector = Vector2.up;
         player = GameObject.FindGameObjectWithTag(Tags.Player);
         aiCooldownTimer = aiCooldownTime;
 	}
@@ -58,13 +70,24 @@ public class EnemyAI : MonoBehaviour
     {
         if (aiCooldownTimer <= 0)
         {
-            ++actionCounts;
             // time to make another action
             EvaluteGameState();
             currentAction = Act(currentState);
             switch (currentAction)
             {
+                case AiAction.GoForward:
+                    movementVector = facingVector;
+                    break;
+                case AiAction.TurnLeft:
+                    facingVector = Quaternion.Euler(0f, 0f, 90f) * facingVector;
+                    movementVector = facingVector;
+                    break;
+                case AiAction.TurnRight:
+                    facingVector = Quaternion.Euler(0f, 0f, -90f) * facingVector;
+                    movementVector = facingVector;
+                    break;
                 case AiAction.DoNothing:
+                    movementVector = Vector2.zero;
                     break;
             }
             // reset the timer
@@ -74,6 +97,9 @@ public class EnemyAI : MonoBehaviour
         {
             aiCooldownTimer -= Time.deltaTime;
         }
+
+
+        transform.Translate(movement.moveSpeed * movementVector * Time.deltaTime);
 	}
 
     private void EvaluteGameState()
@@ -85,10 +111,19 @@ public class EnemyAI : MonoBehaviour
 
         var x = currentState.tilePos.x;
         var y = currentState.tilePos.y;
-        currentState.canMoveDown = grid.IsWalkableTile(x, y - 1);
-        currentState.canMoveUp = grid.IsWalkableTile(x, y + 1);
-        currentState.canMoveLeft = grid.IsWalkableTile(x - 1, y);
-        currentState.canMoveRight = grid.IsWalkableTile(x + 1, y);
+
+
+        float raycastDistance = 1.1f;
+
+        currentState.canMoveForward = ! Physics2D.Raycast(transform.position, facingVector, raycastDistance, LayerMask.GetMask("Wall"));
+        currentState.canMoveBack = ! Physics2D.Raycast(transform.position, -facingVector, raycastDistance, LayerMask.GetMask("Wall"));
+        currentState.canMoveLeft = ! Physics2D.Raycast(transform.position, Quaternion.Euler(0, 0, 90) * facingVector, raycastDistance, LayerMask.GetMask("Wall"));
+        currentState.canMoveRight =  ! Physics2D.Raycast(transform.position, Quaternion.Euler(0, 0, -90) * facingVector, raycastDistance, LayerMask.GetMask("Wall"));
+
+        Debug.DrawRay(transform.position, facingVector * raycastDistance, Color.red);
+        Debug.DrawRay(transform.position, -facingVector * raycastDistance, Color.blue);
+        Debug.DrawRay(transform.position, Quaternion.Euler(0, 0, 90) * facingVector * raycastDistance, Color.green);
+        Debug.DrawRay(transform.position, Quaternion.Euler(0, 0, -90) * facingVector * 1.1f, Color.magenta);
     }
 
     private AiAction Act(GameStateEvaluation currentState)
@@ -96,19 +131,28 @@ public class EnemyAI : MonoBehaviour
         float randomChoice = Random.Range(0f, 1f);
         if (randomChoice < attitude.GoForwardRatio)
         {
-            return AiAction.GoForward;
+            if (currentState.canMoveForward)
+            {
+                return AiAction.GoForward;
+            }
+            else
+            {
+                if (currentState.canMoveLeft && currentState.canMoveRight)
+                {
+                    return Random.Range(0f, 1f) > 0.5f ? AiAction.TurnLeft : AiAction.TurnRight;
+                }
+                else if (currentState.canMoveRight)
+                {
+                    return AiAction.TurnRight;
+                }
+                return AiAction.TurnLeft;
+            }
         }
 
         randomChoice -= attitude.GoForwardRatio;
-        if (randomChoice < attitude.TurnAndMoveRatio)
+        if (randomChoice < (attitude.TurnLeftRatio + attitude.TurnRightRatio))
         {
-            return AiAction.TurnAndMove;
-        }
-
-        randomChoice -= attitude.TurnAndMoveRatio;
-        if (randomChoice < attitude.TurnRatio)
-        {
-            return AiAction.Turn;
+            return Random.Range(0f, 1f) > 0.5f ? AiAction.TurnLeft : AiAction.TurnRight;
         }
 
         return AiAction.DoNothing;
