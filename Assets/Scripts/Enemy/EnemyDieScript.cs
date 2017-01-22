a8c64f4c040c6b7c8bc345323dcbaac4f6691ab3
Danielle Purdes <daniellepurdes@gmail.com>
2017-01-21 20:13:29 -0600

    enemy deactivates when hit with harpoon

---
+++ Assets/Scripts/Enemy/EnemyDieScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDieScript : MonoBehaviour
+{
+
+	public void Die ()
+	{
+		Debug.Log("woah mama");
+		gameObject.SetActive(false);
+	}
+
+}
