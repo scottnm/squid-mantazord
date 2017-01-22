d34837947bcab798b8585273da9105f41a364c71
Danielle Purdes <daniellepurdes@gmail.com>
2017-01-21 22:20:57 -0600

    added player health and enemy contact depletes health

---
+++ Assets/Scripts/Player/PlayerHealthScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthScript : MonoBehaviour {
+
+	public int health = 3;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionStay2D (Collision2D otherCollider)
+	{
+		GameObject otherGO = otherCollider.collider.gameObject;
+
+		if (otherGO.tag == "Enemy")
+		{
+			health -= 1;
+		}
+
+		if (health <= 0)
+		{
+			gameObject.SetActive(false);
+		}
+	}
+}
